37c3c1eb0f378b313259f2dc9f8e19bb4616c7a3
Erik Mortimer <erikmortimermail@gmail.com>
2021-02-16 09:00:44 -0500

    Changed store to hold products, added buy button func

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore } from 'vuex';
+import filterProducts from '../products/filter-products';
 
 const store = createStore({
   state() {
     return {
       user: null,
+      // productFilter: null, Shouldn't be in the store, since it is specific
+      // to the product component
       products: [{
         id: '24ab7b14-f935-44c1-b91b-8598123ea54a',
         title: 'Headlight Helmet',
@@ -40,11 +43,38 @@ const store = createStore({
         description: 'Everything you need for traction on that thrilling ice climb.',
         image: 'mega-pokey-kit.jpg',
       }],
+      cartItems: [{
+        id: '24ab7b14-f935-44c1-b91b-8598123ea54a',
+        title: 'Headlight Helmet',
+        price: 59.95,
+        category: 'Hiking',
+        description: 'Protect your noggin and light your way. Get the best of both worlds with this 300 lumen lighted helmet.',
+        image: 'headlight-helmet.jpg',
+      }, {
+        id: 'cebbc5ba-f49a-4708-b3dc-51a346b3231e',
+        title: 'Ultimate Dreamline Kayak',
+        price: 649.99,
+        category: 'Kayaking',
+        description: 'Carbon fiber body with platinum hooks make this the perfect gift for that special someone.',
+        image: 'ultimate-dreamline-kayak.jpg',
+      }],
     };
+  },
+  getters: {
+    getFilteredProducts(state) {
+      // This getter is returning a function with the parameter "filter"
+      return (filter) => filterProducts(filter, state.products);
+    },
+    getCartItems(state) {
+      return state.cartItems;
+    },
   },
   mutations: {
     setUser(state, user) {
       state.user = user;
+    },
+    addItemToCart(state, item) {
+      state.cartItems.push(item);
     },
   },
 });
